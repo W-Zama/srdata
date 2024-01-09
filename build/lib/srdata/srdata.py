fcee76d5e30921dbc7b7ed751e7c1428e4f07ecf
W-Zama <wzama4667@gmail.com>
2024-01-09 20:27:31 +0900

    グラフ表示機能の修正

--- build/lib/srdata/srdata.py
+++ build/lib/srdata/srdata.py
@@ -2,6 +2,7 @@ import pandas as pd
 import pkgutil
 import io
 import os
+from matplotlib import pyplot as plt
 
 package_name = __name__.split('.')[0]
 
@@ -15,7 +16,7 @@ def get_dataset(file_names):
         data = pkgutil.get_data(package_name, 'Dataset' + '/' + file_name)
         data_str = data.decode('utf-8')
         data_io = io.StringIO(data_str)
-        dataset = pd.read_csv(data_io)
+        dataset = pd.read_csv(data_io, index_col=0)
         datasets.append(dataset)
 
     # 入力が単一の文字列だった場合、リストではなく単体のデータセットを返す
@@ -48,3 +49,38 @@ def display_dataset_tree(directory_path=os.path.dirname(os.path.abspath(__file__
                 print(f"{indent}└─ {item}")
             else:
                 print(f"{indent}├─ {item}")
+
+def show_graph(file_name, step=False, grid=True, **kwargs):
+    data = get_dataset(file_name)
+    plt.figure()
+
+    if step:
+        plt.step(data.index.values, data.iloc[:, 0].values, **kwargs)
+    else:
+        plt.plot(data.index.values, data.iloc[:, 0].values, **kwargs)
+
+    if grid:
+        plt.grid()
+    plt.xlabel(data.index.name)
+    plt.ylabel(data.columns[0])
+    plt.show()
+
+def show_cum_graph(file_name, step=False, grid=True, **kwargs):
+    data = get_dataset(file_name)
+    cum_data = data # copy
+    new_row = pd.DataFrame({data.columns[0]: [0]}, pd.Index([0], name=data.index.name)) # 時刻0で発見数0
+    cum_data = pd.concat([new_row, cum_data], axis=0)
+    cum_data.iloc[:, 0] = cum_data.iloc[:, 0].cumsum()  # 累積和を計算
+
+    plt.figure()
+
+    if step:
+        plt.step(cum_data.index.values, cum_data.iloc[:, 0].values, **kwargs)
+    else:
+        plt.plot(cum_data.index.values, cum_data.iloc[:, 0].values, **kwargs)
+
+    if grid:
+        plt.grid()
+    plt.xlabel(cum_data.index.name)
+    plt.ylabel("cumlative_" + cum_data.columns[0])
+    plt.show()
